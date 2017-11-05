07140770ea21f7a201fb9055675ee925facaee46
bcyan <448162474@qq.com>
2017-11-05 07:27:08 +0000

    unfinished,should have a check

--- infrasim/monitor/__init__.py
+++ infrasim/monitor/__init__.py
@@ -5,12 +5,20 @@ from .apis import api
 from .apis.qmp import ns as qmp_ns
 from .apis.hmp import ns as hmp_ns
 from .apis.admin import ns as admin_ns
-
+from monitor import monitor_logger
+import logging
 
 app = Flask(__name__)
 
+def start(instance=instance, host=host, port=port):
+    api.add_namespace(qmp_ns)
+    api.add_namespace(hmp_ns)
+    api.add_namespace(admin_ns)
 
-api.add_namespace(qmp_ns)
-api.add_namespace(hmp_ns)
-api.add_namespace(admin_ns)
-api.init_app(app)
+    mlog = monitor_logger.init_logger(instance)
+
+    app.logger.addHandler(mlog)
+    app.logger.setLevel(logging.DEBUG)
+
+    app.run(host=host, port=port)
+    api.init_app(app)
